82becec46a1993204b68b197b0e6950c11a16d97
Sekwon Choi <schoi@netflix.com>
2015-10-02 10:02:05 -0700

    todolist

---
+++ ReactStarter-1.2/src/app.jsx
@@ -0,0 +1,33 @@
+var React = require('react');
+var ReactFire = require('reactfire'); // comm between readct and data from firebase
+var Firebase = require('firebase'); // comm with online database
+var Header = require('./header');
+var rootUrl = 'https://flickering-fire-7116.firebaseio.com/';
+
+var App = React.createClass({
+  // react lifecycle methods - componentWillMount
+  // Invoked once, both on the client and server, immediately before the initial rendering occurs.
+  // we want to create firebase object one time
+  mixins: [ReactFire],
+  componentWillMount: function() {
+    // bindAsObject - reactfire method (available via mixins)
+    // mixins of ReactFire copies ReacFire methods to this component
+    this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items');
+    // after this, anything found under url/items/ will be seen as this.state.items => {}
+    // whenever this changes, it will cause re-render
+  },
+  render: function() {
+    console.log(this.state);
+    return <div className="row panel panel-default">
+      <div className="col-md-8 col-mo-offset-2">
+        <h2 className="text-center">
+          To-Do list
+        </h2>
+        <Header/>
+      </div>
+    </div>
+  }
+});
+
+var element = React.createElement(App, {});
+React.render(element, document.querySelector('.container'));
---
+++ ReactStarter-1.2/src/header.jsx
@@ -0,0 +1,39 @@
+var React = require('react');
+
+
+// how to work with form - controlled/uncotrolled
+
+module.exports = React.createClass({
+  getInitialState: function() {
+    return {
+      text:''
+    }
+  },
+  render: function() {
+    return <div className="input-group">
+      <input
+        value={this.state.text}
+        onChange={this.handleInputChange}
+        type="text"
+        className="form-control" />
+      <span className="input-group-btn">
+        <button
+          onClick={this.handleClick}
+          className="btn btn-default"
+          type="button">
+          Add
+        </button>
+      </span>
+      </div>
+  },
+  handleClick: function() {
+    console.log(this.state.text);
+    // send value of the text input to firebase
+  },
+  handleInputChange: function(event) {
+    // DOM reference. In this case, input
+    // every DOM property has value as member
+    //console.log(event.target.value);
+    this.setState({text: event.target.value});
+  }
+});
